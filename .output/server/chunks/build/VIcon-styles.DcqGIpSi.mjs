17f741feb6f1d5e6445797f614b128e55701f33e
Nûr <djedidinur@gmail.com>
2025-01-21 08:21:24 +0100

    release

--- .output/server/chunks/build/VIcon-styles.DcqGIpSi.mjs
+++
@@ -1,6 +0,0 @@
-import { V as VIcon } from './entry-styles-4.mjs-CWoX264H.mjs';
-
-const VIconStyles_DcqGIpSi = [VIcon];
-
-export { VIconStyles_DcqGIpSi as default };
-//# sourceMappingURL=VIcon-styles.DcqGIpSi.mjs.map
--- .output/server/chunks/build/index-styles.CMAq02xd.mjs
+++
@@ -1,6 +0,0 @@
-import { i as index_vue_vue_type_style_index_0_lang } from './index-styles-1.mjs-ClE2F2Yf.mjs';
-
-const indexStyles_CMAq02xd = [index_vue_vue_type_style_index_0_lang];
-
-export { indexStyles_CMAq02xd as default };
-//# sourceMappingURL=index-styles.CMAq02xd.mjs.map
