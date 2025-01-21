f03602dd80dc49f9de7b4f990470127ebcfe278c
Nûr <djedidinur@gmail.com>
2025-01-21 09:05:53 +0100

    release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/urbanstyle',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   plugins: ['~/plugins/cart.js'],
